893180b91ef04e4c9416e92466b8432720b8b58b
dwkeis <dwkeis@gmail.com>
2020-11-16 20:47:40 +0800

    Update fries.py
    pylint ed

--- fries.py
+++ fries.py
@@ -1,27 +1,32 @@
-class fries():
-	def describe(self):
-		pass
+"""is a test for factory model"""
+class Fries():
+    """define fries"""
+    def describe(self):
+        pass
 
-class noSalt(fries):
-	def __init__(self,flavor):
-		self.__name = flavor
-	def describe(self):
-		print("i am " + self.__name + " fries")
+class NoSalt(Fries):
+    """define fries without salt"""
+    def __init__(self,flavor):
+        self.__name = flavor
+    def describe(self):
+        print("i am " + self.__name + " fries")
 
-class default(fries):
-	def __init__(self,flavor):
-		self.__name = flavor
-	def describe(self):
-		print("i am " + self.__name + " fries")
+class Default(Fries):
+    """ it's the default  flavor of fries"""
+    def __init__(self,flavor):
+        self.__name = flavor
+    def describe(self):
+        print("i am " + self.__name + " fries")
 
 class Factory:
-	def product_fries(self, flavor):
-		if flavor == 'nosalt':
-			return noSalt("no salt")
-		if flavor == 'original':
-			return default("default")
+    """where to make fries"""
+    def product_fries(self, flavor):
+        if flavor == 'nosalt':
+            return NoSalt("no salt")
+        if flavor == 'original':
+            return Default("default")
 
 if __name__ == '__main__':
-	factory = Factory()
-	factory.product_fries('nosalt').describe()
-	factory.product_fries('original').describe()
+    factory = Factory()
+    factory.product_fries('nosalt').describe()
+    factory.product_fries('original').describe()
